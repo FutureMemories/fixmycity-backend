617b15a678e192bedeb27c33f306a594edfe836e
Andreas Kaltenbach <andreas@futurememories.se>
2016-05-14 14:45:16 +0200

    avoiding null comments

--- grails-app/controllers/fixmycity/backend/IssueController.groovy
+++ grails-app/controllers/fixmycity/backend/IssueController.groovy
@@ -44,18 +44,21 @@ class IssueController extends RestfulController {
         respond issues.collect { i ->
             def pos = ["long": i.longitude, "lat": i.latitude]
 
-            def imageUrl = null
-            if (i.issueImage != null && i.issueImage.length > 0) {
-                imageUrl = "http://192.81.222.241:8080/issues/" + i.id + "/image"
+
+
+            def res = ["type"    : i.issueType,
+                       "category": i.issueCategory,
+                       "position": pos
+            ]
+
+            if (i.comment != null) {
+                res["comment"] = i.comment
             }
 
-            ["type"    : i.issueType,
-             "category": i.issueCategory,
-             "comment" : i.comment,
-             "position": pos,
-             "imageUrl": imageUrl
-
-            ]
+            if (i.issueImage != null && i.issueImage.length > 0) {
+                res["imageUrl"] = "http://192.81.222.241:8080/issues/" + i.id + "/image"
+            }
+            return res
         }
 
     }

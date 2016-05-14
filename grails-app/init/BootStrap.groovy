c15325cf338338ae7a86554719d8124ff2c755da
Andreas Kaltenbach <andreas@futurememories.se>
2016-05-14 12:44:11 +0200

    uploading issues

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,5 +1,4 @@
 import fixmycity.backend.Issue
-import fixmycity.backend.Location
 
 class BootStrap {
 
@@ -8,13 +7,15 @@ class BootStrap {
         new Issue(issueType: "Problem",
                 issueCategory: "Annat problem",
                 comment: "Bänken är monterad på fel håll",
-                position: new Location(latitude: 57.70350521, longitude: 11.97080612)
+                latitude: 57.70350521,
+                longitude: 11.97080612
         ).save()
 
         new Issue(issueType: "Problem",
                 issueCategory: "Nedskräpning",
                 comment: "Nedskräpning i slottsskogen",
-                position: new Location(latitude: 57.6875885, longitude: 11.94316864)
+                latitude: 57.6875885,
+                longitude: 11.94316864
         ).save()
 
 

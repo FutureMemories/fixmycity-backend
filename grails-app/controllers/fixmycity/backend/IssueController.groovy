b1a5053c5e87968c6d87eb3a16a3c4d0fedf5fee
Andreas Kaltenbach <andreas@futurememories.se>
2016-05-14 12:16:30 +0200

    correct url mapping

--- grails-app/controllers/fixmycity/backend/IssueController.groovy
+++ grails-app/controllers/fixmycity/backend/IssueController.groovy
@@ -9,16 +9,26 @@ class IssueController extends RestfulController {
         super(Issue)
     }
 
+    def save() {
+
+        def img = request.getFile('image')
+        println(img.bytes)
+
+        def issue = new Issue(image : img.bytes)
+        issue.save()
+
+
+        def res = ["asd" : 1]
+        respond res
+    }
+
+
+
     def index() {
+        println("Listing")
         def issues = Issue.list()
-
-
-
-
-
         respond issues.collect { i ->
-
-            def pos = ["long": i.position.longitude, "lat" : i.position.latitude]
+            def pos = ["long": i.position.longitude, "lat": i.position.latitude]
 
             ["type"    : i.issueType,
              "category": i.issueCategory,
@@ -27,18 +37,6 @@ class IssueController extends RestfulController {
             ]
         }
 
-    }
-
-    def update() {
-        log.info("update")
-    }
-
-    def show() {
-        log.info("show")
-    }
-
-    def save() {
-        log.info("save")
     }
 
 }
--- grails-app/domain/fixmycity/backend/Issue.groovy
+++ grails-app/domain/fixmycity/backend/Issue.groovy
@@ -11,6 +11,7 @@ class Issue {
 
     Location position
 
+    byte[] image
 
     static constraints = {
     }
